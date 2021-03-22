0c66d76649cfdd5bce2bf3c682477b709deae886
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-03-21 23:51:44 -0700

    about

--- class-05/lab/starter-code/src/views/IndexSections/Hero.js
+++ class-05/lab/starter-code/src/views/IndexSections/Hero.js
@@ -56,7 +56,7 @@ class Hero extends React.Component {
                     />
                     <h1>Julien Edwards</h1>
                     <p className="lead text-white">
-                      Dynamically rendered to best suit the occasion. 
+                      Design wit hintent. I have had a career developing digital marketing campaigns as well as B2B campaigns, and my taklaway has been get the tools best suited for the job. After brushing up on my design chops and mastering React, I have been working my way to designing my own tools. This ssttention todetail and drive to systematize will get me by.
                     </p>
                     
                     <div className="mt-5">
