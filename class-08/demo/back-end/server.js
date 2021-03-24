62d27584f6f12187a94cd180da9c15d9c89f1e9f
Lena Eivy <applena@gmail.com>
2021-03-24 15:03:09 -0700

    now with the real code

--- class-08/demo/back-end/server.js
+++ class-08/demo/back-end/server.js
@@ -11,6 +11,9 @@ require('dotenv').config();
 const cors = require('cors');
 app.use(cors());
 
+// superagent is going to get us data
+const superagent = require('superagent');
+
 const PORT = process.env.PORT || 3002;
 
 // proof of life
@@ -22,8 +25,32 @@ app.get('/representatives', getReps);
 
 function getReps(request, response) {
   console.log(request.query)
+  // { address: '1600 Pennsylvania ave Washington DC ' }
+  const address = request.query.address;
+  const url = 'https://www.googleapis.com/civicinfo/v2/representatives';
+  const query = {
+    address: address,
+    key: process.env.GOOGLE_KEY
+  }
+
+  superagent
+    .get(url)
+    .query(query)
+    .then(superagentResults => {
+      // the thing we care about with superagent is the results.body
+      // this is where I would run the data through a constructor function, if I wanted to shape the data
+      const repArray = superagentResults.body.map(agent => {
+        new Representatives(agent);
+      })
+
+      response.status(200).send(repArray);
+    })
   // go to Google Civic API
   // get the reps and send them back to our front end
+}
+
+function Representatives(obj) {
+  this.name = obj.name
 }
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
